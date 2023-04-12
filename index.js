381713883dcefe881a1bb568576222516c0eda49
Tormod <tormod.flesjo@gmail.com>
2023-04-12 10:36:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const onKlikkHerKeyDown = (e) => {
+  if (e) e.preventDefault();
+  window.location.href = "https://mills.no/vita-hjertego/";
+};
+
+/* document.addEventListener(
+  "keydown",
+  (e) => {
+    if (e.keyCode === 9) {
+      if (Math.random() > 0.5) {
+        console.log("hit");
+        e.preventDefault();
+      } else {
+        return;
+      }
+    }
+  },
+  false
+);
+ */
